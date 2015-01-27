8004def50cff56c01d41bbcae84e45925498454a
wangyangchao <827741251@qq.com>
2015-01-27 14:02:06 +0800

    创建用户注册

--- server/src/main/java/com/ld/controller/UserController.java
+++ server/src/main/java/com/ld/controller/UserController.java
@@ -1,5 +1,16 @@
 package com.ld.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 public class UserController {
 
+	public Map<String, Object> userRegister(HttpServletRequest request){
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		return result;
+		
+	}
 }
